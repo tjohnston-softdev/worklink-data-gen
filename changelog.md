7640123cd91190ef5ea3aefaf172d71510152a05
Tyrone Johnston <tyrone.johnston@hotmail.com>
2021-08-01 11:11:25 +1000

    Comments - Password generation

--- changelog.md
+++ changelog.md
@@ -1,11 +1,4 @@
 # Changelog
 
-**./info/options.md**
-* Removed:
-	* userPassword.minCharacters
-	* userPassword.maxCharacters
-* Added:
-	* encryption.enabled
-	* encryption.key
-	* encryption.checkMatch
-* Last Updated: 1 August 2021
+**./src/generation/person-password.js**
+* Wrote comments.
--- src/generation/person-password.js
+++ src/generation/person-password.js
@@ -1,3 +1,5 @@
+// Generates passwords for Support Worker account objects.
+
 const randomTasks = require("../common/random-tasks");
 
 const characterTypes =
@@ -8,23 +10,32 @@ const characterTypes =
 	"SPECIAL": 4
 };
 
+
+// Main function.
 function chooseRandomString(parentObject)
 {
 	var charSlotArray = initializeSlots();
 	var lockedTypeArray = [];
 	
+	// Ensure required amount of characters for each type.
 	setRequiredCharType(charSlotArray, lockedTypeArray, characterTypes.LOWER, 2);
 	setRequiredCharType(charSlotArray, lockedTypeArray, characterTypes.UPPER, 2);
 	setRequiredCharType(charSlotArray, lockedTypeArray, characterTypes.DIGIT, 2);
 	setRequiredCharType(charSlotArray, lockedTypeArray, characterTypes.SPECIAL, 2);
 	
+	// Generate initial password characters.
 	lockedTypeArray = [];
 	setCharacters(charSlotArray);
+	
+	// Replace repeating characters.
 	avoidRepeatCharacters(charSlotArray);
+	
+	// Complete.
 	writeFinalString(charSlotArray, parentObject);
 }
 
 
+// Initialize password object.
 function initializeSlots()
 {
 	var chosenLength = randomTasks.rollInteger(8, 32);
@@ -34,8 +45,10 @@ function initializeSlots()
 	
 	var intlRes = [];
 	
+	// Generate chosen amount of password character slots.
 	while (intlRes.length < chosenLength)
 	{
+		// Choose random type.
 		currentType = randomTasks.rollInteger(1, 4);
 		currentSlot = defineCharSlot(currentType);
 		intlRes.push(currentSlot);
@@ -45,6 +58,7 @@ function initializeSlots()
 }
 
 
+// Ensures that a minimum amount of characters in a group will be included.
 function setRequiredCharType(slotArray, lockArray, charTypeFlag, reqCount)
 {
 	var setCount = 0;
@@ -52,14 +66,17 @@ function setRequiredCharType(slotArray, lockArray, charTypeFlag, reqCount)
 	var currentLocked = false;
 	var currentSlotObject = {};
 	
+	// Loop until requirement met, or all slots locked.
 	while (setCount < reqCount && lockArray.length < slotArray.length)
 	{
+		// Choose random character slot.
 		currentSlotIndex = randomTasks.rollElement(slotArray.length);
 		currentLocked = lockArray.includes(currentSlotIndex);
 		currentSlotObject = {};
 		
 		if (currentLocked !== true)
 		{
+			// Allocate character type, and lock to prevent changes.
 			currentSlotObject = slotArray[currentSlotIndex];
 			currentSlotObject.type = charTypeFlag;
 			lockArray.push(currentSlotIndex);
@@ -70,6 +87,7 @@ function setRequiredCharType(slotArray, lockArray, charTypeFlag, reqCount)
 }
 
 
+// Generates initial set of characters for the password.
 function setCharacters(slotArray)
 {
 	var slotIndex = 0;
@@ -78,6 +96,7 @@ function setCharacters(slotArray)
 	
 	for (slotIndex = 0; slotIndex < slotArray.length; slotIndex = slotIndex + 1)
 	{
+		// Set current slot's character.
 		currentSlot = slotArray[slotIndex];
 		currentChar = decideCharacter(currentSlot.type);
 		currentSlot.character = currentChar;
@@ -85,6 +104,7 @@ function setCharacters(slotArray)
 }
 
 
+// Ensures that no character repeats more than twice in a row.
 function avoidRepeatCharacters(slotArray)
 {
 	var slotIndex = 0;
@@ -97,6 +117,7 @@ function avoidRepeatCharacters(slotArray)
 	var iterationValid = true;
 	var repeatSafe = false;
 	
+	// Outer loop until repetition is safe.
 	while (repeatSafe !== true)
 	{
 		slotIndex = 0;
@@ -108,16 +129,22 @@ function avoidRepeatCharacters(slotArray)
 		
 		iterationValid = true;
 		
+		// Inner loop reads string for repetition.
 		while (slotIndex >= 0 && slotIndex < slotArray.length && iterationValid === true)
 		{
+			// Read current character.
 			currentSlot = slotArray[slotIndex];
 			currentOrigin = currentSlot.character;
+			
+			// Read following characters.
 			currentNextA = getOffsetCharacter(slotArray, slotIndex, 1);
 			currentNextB = getOffsetCharacter(slotArray, slotIndex, 2);
 			currentReplacement = "";
 			
+			
 			if (currentOrigin === currentNextA && currentOrigin === currentNextB)
 			{
+				// Fail - Choose different character for current slot.
 				currentReplacement = decideCharacter(currentSlot.type);
 				currentSlot.character = currentReplacement;
 				iterationValid = false;
@@ -131,22 +158,27 @@ function avoidRepeatCharacters(slotArray)
 }
 
 
+// Writes final password string from slot array.
 function writeFinalString(slotArray, parentObj)
 {
 	var slotIndex = 0;
 	var currentSlot = {};
 	var finalString = "";
 	
+	// Read characters from slots.
 	for (slotIndex = 0; slotIndex < slotArray.length; slotIndex = slotIndex + 1)
 	{
+		// Add current character.
 		currentSlot = slotArray[slotIndex];
 		finalString += currentSlot.character;
 	}
 	
+	// Add to account object.
 	parentObj.push(finalString);
 }
 
 
+// Reads following slot character based on offset.
 function getOffsetCharacter(charArray, originInd, offsetInd)
 {
 	var targetIndex = originInd + offsetInd;
@@ -155,6 +187,7 @@ function getOffsetCharacter(charArray, originInd, offsetInd)
 	
 	if (targetIndex >= 0 && targetIndex < charArray.length)
 	{
+		// Read character slot.
 		offsetSlot = charArray[targetIndex];
 		charRes = offsetSlot.character;
 	}
@@ -163,6 +196,7 @@ function getOffsetCharacter(charArray, originInd, offsetInd)
 }
 
 
+// Generates random character based on type.
 function decideCharacter(slotType)
 {
 	var charRes = "";
@@ -188,6 +222,7 @@ function decideCharacter(slotType)
 }
 
 
+// Password character slot object.
 function defineCharSlot(typeFlag)
 {
 	var defineRes = {};
